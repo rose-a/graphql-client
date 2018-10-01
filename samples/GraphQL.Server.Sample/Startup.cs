7e6d88e0a2d4993fa34231ab4caf8b39f411ae86
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-10-01 18:17:36 +0200

    initial server

---
+++ samples/GraphQL.Server.Sample/Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQL.Server.Sample {
+
+	public class Startup {
+
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration) {
+			Configuration = configuration;
+		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services) {
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+		}
+
+	}
+
+}
